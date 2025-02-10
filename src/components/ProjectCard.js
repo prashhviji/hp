ba535c264149f290321126153d205f6153df590a
Hariprasaadh <hariprasaadh.k2023@vitstudent.ac.in>
2025-02-10 22:57:33 +0530

    update projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,7 +6,7 @@ export const ProjectCard = ({ title, description, imgUrl, githubLink, deployLink
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <img src={imgUrl} />
+        <img src={imgUrl} className="proj-img" />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
