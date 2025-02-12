0cf262939f0d77270c23ed2c0e6426c266deee70
Hariprasaadh <hariprasaadh.k2023@vitstudent.ac.in>
2025-02-13 00:30:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ console.log(process.env.EMAIL_PASS);
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: "********@gmail.com",
-    pass: ""
+    user: "hari.kdh7376@gmail.com",
+    pass: "rsnb yobo ddku phrf"
   },
 });
 
